05f2dbc7118e369d3a60b310f4ded7d1edf88b36
JulienRebillon <julienrebillon@yahoo.fr>
2024-10-22 18:18:09 +0200

    fix css & tooltip lineChart.

--- src/Components/lineChart/lineChart.jsx
+++ src/Components/lineChart/lineChart.jsx
@@ -5,7 +5,7 @@
 //          fetchUserAverageSessions,
 //          fetchUserPerformance } from '../Data/api';
 
-import React, { useState } from "react";
+import React from "react";
 import {
     Line,
     LineChart,
@@ -22,34 +22,13 @@ const fakeData = [
     {
         "userId": 12,
         "sessions": [
-            {
-                "day": 1,
-                "sessionLength": 30
-            },
-            {
-                "day": 2,
-                "sessionLength": 23
-            },
-            {
-                "day": 3,
-                "sessionLength": 45
-            },
-            {
-                "day": 4,
-                "sessionLength": 50
-            },
-            {
-                "day": 5,
-                "sessionLength": 0
-            },
-            {
-                "day": 6,
-                "sessionLength": 0
-            },
-            {
-                "day": 7,
-                "sessionLength": 60
-            }
+            { "day": 1, "sessionLength": 30 },
+            { "day": 2, "sessionLength": 23 },
+            { "day": 3, "sessionLength": 45 },
+            { "day": 4, "sessionLength": 50 },
+            { "day": 5, "sessionLength": 0 },
+            { "day": 6, "sessionLength": 0 },
+            { "day": 7, "sessionLength": 60 }
         ]
     }
 ];
@@ -57,51 +36,6 @@ const fakeData = [
 const sessionData = fakeData[0].sessions;
 
 
-const fakeData3 = [
-    {
-      value: 80,
-      kind: "cardio"
-    },
-    {
-      value: 120,
-      kind: "energy"
-    },
-    {
-      value: 140,
-      kind: "endurance"
-    },
-    {
-      value: 50,
-      kind: "strength"
-    },
-    {
-      value: 200,
-      kind: "speed"
-    },
-    {
-      value: 90,
-      kind: "intensity"
-    }
-  ];
-
-
-  const fakeData4 = [
-    {
-        "id": "12",
-        "userInfos": {
-            "firstName": "Karl",
-            "lastName": "Dovineau",
-            "age": 31
-        },
-        "todayScore": 0.12,
-        "keyData": {
-            "calorieCount": 1930,
-            "proteinCount": 155,
-            "carbohydrateCount": 290,
-            "lipidCount": 50
-        }
-    },
-]
   
 
 
@@ -109,17 +43,75 @@ const SimpleLineChart = () => {
 
 
 
+    function showOverlay(e) {
+        let overlay = document.querySelector(".lineChart_overlay");
+  
+        if (e.isTooltipActive) {
+          let windowWidth = overlay.offsetWidth;
+          let mouseXpercent = Math.floor(
+            (e.activeCoordinate.x / windowWidth) * 100
+          );
+  
+          overlay.style.background = `linear-gradient(to right, rgb(255,0,0) ${mouseXpercent}%, rgba(0,0,0,0.1) ${mouseXpercent}%`;
+        } else {
+          overlay.style.background = "transparent";
+        }
+      }
+  
+    function hiddeOverlay() {
+        let overlay = document.querySelector(".lineChart_overlay");
+        overlay.style.background = "transparent";
+    }
+
+    const CustomTooltip = ({ active, payload, label, coordinate }) => {
+        if (active && payload && coordinate) {
+            return (
+                <div className="lineChart_tooltip"
+                style={{
+                    position: "absolute",
+                    left: `${coordinate.x + 10}px`, // Adjusting to the right of the hovered point
+                    top: `${coordinate.y - 40}px`, // Adjusting above the hovered point
+                    transform: "translate(0, 0)", // Resets any transform effects
+                }}>
+                <p>{`${payload[0].value} min`}</p>
+                </div>
+            );
+        }
+      
+        return null;
+      };
+      
+      
+
+    const dayLabels = ["", "L", "M", "M", "J", "V", "S", "D"]; // Days of the week (empty string to match index with 'day' value)
+
     return (
         <div className="graphs_content">
 
+            <div className="lineChart_legend">
+                <span>durée moyenne des</span> <span>sessions</span>
+            </div>
+
             <div className="miniGraph" id="graph1">
+                <div className="lineChart_overlay"></div>
                 <ResponsiveContainer width="100%" height="100%">
+                    
                     <LineChart data={sessionData}
-                        margin={{ top: 20, right: 30, left: 0, bottom: 0 }}>
-                        <XAxis dataKey="day" />
+                        margin={{ top: 20, right: 0, left: 0, bottom: 20 }}
+                        onMouseMove={(e) => showOverlay(e)} onMouseOut={hiddeOverlay}>
+                        <XAxis 
+                            dataKey="day"
+                            tick={{ fill: 'white' }}
+                            axisLine={{ stroke: 'white' }}
+                            interval={0}
+                            padding={{ left: 10, right: 10 }}
+                            fontSize={14}
+                            fontWeight={300}
+                            tickFormatter={(day) => dayLabels[day]} // Maps the numbers to day initials
+                        />
                         <YAxis hide={true} domain={[0, (max) => max * 1.33]} dataKey="sessionLength"/>
-                        <Tooltip />
-                        <Line type="monotone" dataKey="sessionLength" stroke="white" strokeWidth={2} dot={{ r: 5 }} />
+                        <Tooltip content={<CustomTooltip />} />
+                        <Line type="monotone" dataKey="sessionLength" stroke="white" strokeWidth={2} dot={ false } />
                     </LineChart>
                 </ResponsiveContainer>
             </div>
